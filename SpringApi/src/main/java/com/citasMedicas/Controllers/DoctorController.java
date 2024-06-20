14f628646140ec870c1f63296f0c23b7cf587041
juniorDeveloper8 <be6323787@gmail.com>
2024-06-20 14:41:03 -0500

    agrego endpoints y la conexion con la base de datos

--- SpringApi/src/main/java/com/citasMedicas/Controllers/DoctorController.java
+++ SpringApi/src/main/java/com/citasMedicas/Controllers/DoctorController.java
@@ -5,11 +5,11 @@ import com.citasMedicas.Entities.User;
 import com.citasMedicas.Services.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -17,11 +17,12 @@ import java.util.Optional;
 public class DoctorController {
 
     @Autowired
-    private IUserService userService;
+    private IUserService doctor;
+
 
     @GetMapping(value = "/doctor/{id}")
     public ResponseEntity<?> findByIdDoctor(@PathVariable Integer id) {
-        Optional<User> userOptional = userService.findById(id);
+        Optional<User> userOptional = doctor.findById(id);
 
         if(userOptional.isPresent()) {
             User user = userOptional.get();
@@ -37,5 +38,74 @@ public class DoctorController {
             return ResponseEntity.ok(dtoUser);
         }
         return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping(value = "/doctor")
+    public ResponseEntity<?> findAll() {
+
+        try {
+            List<DTOUser> dtoUserList = doctor.findAll().stream()
+                    .map(doc -> DTOUser.builder()
+                            .uNombre(doc.getUNombre())
+                            .uApellido(doc.getUApellido())
+                            .uCorreo(doc.getUCorreo())
+                            .identificacion(doc.getIdentificacion())
+                            .docEspecialidadList(doc.getDocEspecialidadList())
+                            .build()
+                    )
+                    .toList();
+            return ResponseEntity.ok(dtoUserList);
+
+        } catch (Exception e) {
+
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping(value = "/doctor")
+    public ResponseEntity<?> save(@RequestBody DTOUser dtoUser) throws URISyntaxException {
+
+         if(dtoUser.getUNombre().isBlank() || dtoUser.getUApellido().isBlank() || dtoUser.getUCorreo().isBlank() || dtoUser.getUPsw().isBlank() || dtoUser.getIdentificacion() == null) {
+           return ResponseEntity.badRequest().build();
+       }
+
+         User user = User.builder()
+                 .uNombre(dtoUser.getUNombre())
+                 .uApellido(dtoUser.getUApellido())
+                 .uCorreo(dtoUser.getUCorreo())
+                 .uPsw(dtoUser.getUPsw())
+                 .identificacion(dtoUser.getIdentificacion())
+                 .docEspecialidadList(dtoUser.getDocEspecialidadList())
+                 .build();
+         doctor.save(user);
+
+         return ResponseEntity.created(new URI("/api/doctor")).build();
+    }
+
+    @PutMapping(value = "/doctor/{id}")
+    public ResponseEntity<?> update(@PathVariable Integer id, @RequestBody DTOUser dtoUser) {
+        Optional<User> userOptional = doctor.findById(id);
+
+        if(userOptional.isPresent()){
+            User user = userOptional.get();
+            user.setUNombre(dtoUser.getUNombre());
+            user.setUApellido(dtoUser.getUApellido());
+            user.setUCorreo(dtoUser.getUCorreo());
+            user.setIdentificacion(dtoUser.getIdentificacion());
+            user.setDocEspecialidadList(dtoUser.getDocEspecialidadList());
+            doctor.save(user);
+
+            return ResponseEntity.ok("doctor actualizado");
+        }
+        return ResponseEntity.badRequest().build();
+    }
+
+    @DeleteMapping(value = "/doctor/{id}")
+    public ResponseEntity<?> delete(@PathVariable Integer id) {
+        if (id != null) {
+            doctor.deleteById(id);
+            return ResponseEntity.ok("doctor eliminado");
+        }
+        return ResponseEntity.badRequest().build();
     }
 }
